dd5ca39a74b2bfbb270622acf08332270cd8fadc
dhruvbharija8-lang <dhruvbharija8@gmail.com>
2026-09-14 02:08:59 +0530

    Time: 115 ms (19.10%) | Memory: 91.6 MB (6.00%) - LeetSync

--- 1206-corporate-flight-bookings/corporate-flight-bookings.cpp
+++ 1206-corporate-flight-bookings/corporate-flight-bookings.cpp
@@ -2,8 +2,11 @@ class Solution {
 public:
     vector<int> corpFlightBookings(vector<vector<int>>& bookings, int n) {
 
-     vector<int>mp(n+2,0);
-        
+        map<long long,long long>mp;
+        for(int i=1;i<=n;i++){
+            mp[i]=0;
+        }
+
         for(auto book:bookings){
             int start=book[0];
             int end=book[1];
@@ -13,15 +16,19 @@ public:
 
         }
       
-        vector<int>res(n);
-          int sum = 0;
+        vector<int>res;
+        long long sum=0;
+        long long prev=mp.begin()->first;
+        for(auto it=mp.begin();it!=mp.end();it++){
+            long long point=it->first;
+            long long change=it->second;
+          sum += change; 
 
-        for(int i = 1; i <= n; i++) {
-            sum += mp[i];
-            res[i - 1] = sum;
+            if(point <= n) {
+                res.push_back(sum); 
+
         }
-
-        
+        }
   return res;
         
     }
